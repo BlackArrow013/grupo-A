73934c0ed56f5091ae7194366ac1e56c47a389e2
Jorge Jaular Lasaga <jordijaulas13@gmail.com>
2017-11-17 16:56:25 +0100

    Implementado el método infoDeMails, un contador de mails enviados y recibidos, y que muestra el numero de caracteres del mensaje más largo y el emisor del mensaje.

--- MailClient.java
+++ MailClient.java
@@ -11,12 +11,14 @@ public class MailClient
     private MailServer server;
     // The user running this client.
     private String user;
-    // El nÃºmero de mails enviados.
+    // El número de mails enviados.
     private int enviados;
-    // El nÃºmero de mails recibidos.
+    // El número de mails recibidos.
     private int recibidos;
-    // El nÃºmero de caracteres del mensaje mÃ¡s largo.
+    // El número de caracteres del mensaje más largo.
     private int caracteresMensajeMasLargo;
+    // El emisor del mensaje más largo.
+    private String emisor;
 
     /**
      * Create a mail client run by user and attached to the given server.
@@ -25,9 +27,14 @@ public class MailClient
     {
         this.server = server;
         this.user = user;
+        // el atributo enviados se inicializa a 0.
         enviados = 0;
+        // el atributo recibidos se inicializa a 0.
         recibidos = 0; 
-        caracteresMensajeMasLargo = 0;
+        // el atributo que cuenta los caracteres del mensaje más largo recibido se inicializa a 0.
+        caracteresMensajeMasLargo = 0; 
+        // el atributo encargado del emisor responsable del mensaje con más caracteres se inicializa con un nadie en caso de que no se haya recibido ningún mensaje.
+        emisor = "nadie";
     }
 
     /**
@@ -37,9 +44,14 @@ public class MailClient
     {        
         MailItem item = server.getNextMailItem(user);
         if (item != null) {
+            // Suma al contador de mensajes recibidos uno cada vez que se imprime uno por pantalla.
             recibidos += 1;
+            // Tras sumar un mensaje recibido se compara el número de caracteres del mensaje con el atributo caracteresMensajeMasLargo inicializado a 0.
             if (item.getMessage().length() > caracteresMensajeMasLargo) {
+                // Sustituye los caracteres del mensaje por la inicialización del atributo, y lo guarda para compararlo con posteriores mensajes.
                 caracteresMensajeMasLargo = item.getMessage().length();
+                // Además devuelve como emisor a la persona que ha mandado el mensaje con más caracteres, sustituyendo el "nadie" con el que se inicializa el atributo.
+                emisor = item.getFrom();
             }
         }
         return item;
@@ -51,12 +63,17 @@ public class MailClient
      */
     public void printNextMailItem()
     {
-        MailItem item = server.getNextMailItem(user);
+        MailItem item = server.getNextMailItem(user);        
         if (item != null) {
             item.print();
+            // Suma al contador de mensajes recibidos uno cada vez que se imprime uno por pantalla.
             recibidos += 1;
+            // Tras sumar un mensaje recibido se compara el número de caracteres del mensaje con el atributo caracteresMensajeMasLargo inicializado a 0.
             if (item.getMessage().length() > caracteresMensajeMasLargo) {
+                // Sustituye los caracteres del mensaje por la inicialización del atributo, y lo guarda para compararlo con posteriores mensajes.
                 caracteresMensajeMasLargo = item.getMessage().length();
+                // Además devuelve como emisor a la persona que ha mandado el mensaje con más caracteres, sustituyendo el "nadie" con el que se inicializa el atributo.
+                emisor = item.getFrom();
             }                        
         }
         else {
@@ -74,19 +91,23 @@ public class MailClient
     {
         MailItem item = new MailItem(user, to, message, subject);
         server.post(item);
-        
+        // Tras mandar un mensaje, el contador de enviados suma uno más al registro.
         enviados += 1;
     }
     
     /**
-     * Muestra por pantalla el nÃºmero total de mensajes recibidos, numero total de mensajes enviados asÃ­ como la direcciÃ³n de correo de 
-     * la persona que nos envÃ­o el email mÃ¡s largo hasta el momento y el nÃºmero de caracteres del mismo.
+     * Muestra por pantalla el número total de mensajes recibidos, numero total de mensajes enviados así como la dirección de correo de 
+     * la persona que nos envío el email más largo hasta el momento y el número de caracteres del mismo.
      */
     public void infoDeMails()
-    {
+    {              
+        // Muestra la cantidad de mails enviados hasta el momento desde esta cuenta.
         System.out.println("Mails enviados: " + enviados);
+        // Muestra la cantidad de mails recibidos hasta el momento a esta cuenta.
         System.out.println("Mails recibidos: " + recibidos);
-        System.out.println("Persona que me envÃ­a el mail mÃ¡s largo: " + user);
-        System.out.println("NÃºmero de caracteres del mail mÃ¡s largo: " + caracteresMensajeMasLargo);
+        // Indica el nombre del emisor responsable del mensaje con más caracteres.
+        System.out.println("Persona que me envía el mail más largo: " + emisor);
+        // Indica el número de caracteres del mensaje más largo.
+        System.out.println("Número de caracteres del mail recibido más largo: " + caracteresMensajeMasLargo);
     }
 }
